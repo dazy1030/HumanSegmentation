1da0ced47381849b80dec99bf18dc7944aeb7908
dazy1030 <dev.nodajima@gmail.com>
2022-03-30 15:14:22 +0900

    Initial Commit

---
+++ HumanSegmentation/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  HumanSegmentation
+//
+//  Created by Naoki Odajima on 2022/03/30.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        true
+    }
+}
---
+++ HumanSegmentation/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  HumanSegmentation
+//
+//  Created by Naoki Odajima on 2022/03/30.
+//
+
+import UIKit
+
+@MainActor
+final class ViewController: UIViewController {}
